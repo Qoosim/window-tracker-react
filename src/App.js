12123b707aa4c75a252a920ef4e5a9d8d1ac5442
Qoosim <ayinde.abdulghaniyy@gmail.com>
2021-12-17 21:34:57 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import WindowTracker from './components/WindowTracker'
 
 function App() {
+  const [show, setShow] = React.useState(true);
+
+  function handleToggle() {
+    setShow(prevShow => !prevShow)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <button
+        onClick={handleToggle}
+      >
+        Toggle WindowTracker
+      </button>
+      {show && <WindowTracker />}
     </div>
   );
 }
